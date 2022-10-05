750aa01750b8c9f41207dbde9f2be393169488a2
Malcom Green <malcomeqgreen@gmail.com>
2022-10-05 18:26:58 -0400

    Minor adjustments

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className='app dark-theme'>
       <Head>
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#14141C" />
         <meta name="description" content="Design & Development" />
