65d399470707b0cd8b495ba8799f15fcc2a8ffed
Malcom Green <malcomeqgreen@gmail.com>
2022-10-05 12:46:38 -0400

    Added footer

---
+++ components/Footer.js
@@ -0,0 +1,27 @@
+import styles from '../styles/Footer.module.css'
+
+import Image from 'next/image';
+// import { ReactComponent as Logo } from '../icons/Malcom.svg'
+
+const Footer = () => {
+    return (
+        <div className={styles.footer}>
+            <div className={'container'}>
+                <div className={styles.logo}>
+                    <Image
+                        src="/icons/Malcom.svg"
+                        alt='Malcom Logo'
+                        layout='fill'
+                    />
+                </div>
+                <hr />
+                <footer className={styles.copyright}>
+                    <p>&copy; {new Date().getFullYear()} <strong>Malcom Green</strong></p>
+                    <p>Crafted with ❤️ by <strong>Malcom</strong></p>
+                </footer>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
