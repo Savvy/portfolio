b2ae15e3c2e53df44a1da2babf75877df4ba651e
Malcom Green <malcomeqgreen@gmail.com>
2023-04-21 22:04:10 -0400

    Added plausible analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import PlausibleProvider from 'next-plausible'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <PlausibleProvider domain="malcom.sh">
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  )
 }
 
 export default MyApp
